1eb4d6bd90a0ec5a2ce8be522b94b2e770f4b3c6
An0n-00 <142223573+An0n-00@users.noreply.github.com>
2024-08-15 17:36:33 +0200

    update to newest version

--- xpui/dev-playground.js
+++ xpui/dev-playground.js
@@ -1,1 +1,1 @@
-"use strict";(("undefined"!=typeof self?self:global).webpackChunkclient_web=("undefined"!=typeof self?self:global).webpackChunkclient_web||[]).push([[3934],{74038:(e,l,f)=>{f.r(l),f.d(l,{default:()=>s});var n=f(86070);const s=()=>(0,n.jsx)("div",{})}}]);
+"use strict";(("undefined"!=typeof self?self:global).webpackChunkclient_web=("undefined"!=typeof self?self:global).webpackChunkclient_web||[]).push([[3934],{4383:(e,l,f)=>{f.r(l),f.d(l,{default:()=>s});var n=f(86070);const s=()=>(0,n.jsx)("div",{})}}]);
